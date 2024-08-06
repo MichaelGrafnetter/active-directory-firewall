88d80ac12b7a5f22021ec0f202d561adf8303fa2
Michael Grafnetter <michael.grafnetter@outlook.com>
2024-08-06 22:18:27 +0200

    Pages build verbosity

--- Generators/mkdocs/mkdocs.sh
+++ Generators/mkdocs/mkdocs.sh
@@ -7,35 +7,41 @@ ScriptRoot=$(dirname -- "$(readlink --canonicalize -- "$BASH_SOURCE")")
 RepoRoot=$(dirname -- "$(dirname -- "$ScriptRoot")")
 
 # Copy CSS files
-mkdir -p "$RepoRoot/docs/assets/stylesheets"
+mkdir --parents --verbose "$RepoRoot/docs/assets/stylesheets"
 cp "$ScriptRoot/extra.css" "$RepoRoot/docs/assets/stylesheets/extra.css"
 
 # Copy markdown and HTML files
-mkdir -p "$RepoRoot/docs/ADDS"
-cp "$RepoRoot/README.md" "$RepoRoot/docs/"
-cp "$RepoRoot/ADDS/README.md" "$RepoRoot/docs/ADDS/"
-cp "$RepoRoot/ADDS/GPOReport.html" "$RepoRoot/docs/ADDS/"
+mkdir --parents --verbose "$RepoRoot/docs/ADDS"
+cp --verbose "$RepoRoot/README.md" "$RepoRoot/docs/"
+cp --verbose "$RepoRoot/ADDS/README.md" "$RepoRoot/docs/ADDS/"
+cp --verbose "$RepoRoot/ADDS/GPOReport.html" "$RepoRoot/docs/ADDS/"
 
 # Copy all images to the assets directory
-mkdir -p "$RepoRoot/docs/assets/images"
-rsync --archive --no-relative --exclude='*.md' "$RepoRoot/Images/"**/* "$RepoRoot/docs/assets/images"
+mkdir --parents --verbose "$RepoRoot/docs/assets/images"
+rsync --archive --no-relative --exclude='*.md' --verbose "$RepoRoot/Images/"**/* "$RepoRoot/docs/assets/images"
 
 # Fix image paths in markdown files
 # Case 1: Replace paths like ../Images/Screenshots/ with ../assets/images/
-find "$RepoRoot/docs" -name "*.md" -exec sed -i 's/\.\.\/Images\/\w\+\//..\/assets\/images\//g' {} \;
+find "$RepoRoot/docs" -name "*.md" -exec sed --in-place 's/\.\.\/Images\/\w\+\//..\/assets\/images\//g' {} \;
 
 # Case 2: Replace paths like Images/Screenshots/ with assets/images/
-find "$RepoRoot/docs" -name "*.md" -exec sed -i 's/(Images\/\w\+\//(assets\/images\//g' {} \;
+find "$RepoRoot/docs" -name "*.md" -exec sed  --in-place 's/(Images\/\w\+\//(assets\/images\//g' {} \;
 
 # Normalize named anchors in markdown files by replacing 3 consecutive hyphens with a single one
 # Example: Replace #active-directory-domain-controller---ldap-tcp-in with #active-directory-domain-controller-ldap-tcp-in
-find "$RepoRoot/docs" -name "*.md" -exec sed -i 's/\(\w\)---\(\w\)/\1-\2/g' {} \;
+find "$RepoRoot/docs" -name "*.md" -exec sed --in-place 's/\(\w\)---\(\w\)/\1-\2/g' {} \;
 
-# Update the requirements file. MkDocs itself must already be installed.
-mkdocs get-deps --config-file="$ScriptRoot/mkdocs.yml" > "$ScriptRoot/requirements.txt"
+# Update the requirements file
+# Note: MkDocs itself must already be installed.
+mkdocs get-deps --config-file="$ScriptRoot/mkdocs.yml" --verbose > "$ScriptRoot/requirements.txt"
 
 # Install Python dependencies
 pip install --quiet --requirement "$ScriptRoot/requirements.txt"
 
-# Build the site
-mkdocs build --config-file="$ScriptRoot/mkdocs.yml"
+if [[ "$GITHUB_PAGES" != "true" ]]; then
+    # Start a local web server if running outside of GitHub Workflows
+    mkdocs serve --config-file="$ScriptRoot/mkdocs.yml"
+else
+    # Just build the site otherwise
+    mkdocs build --config-file="$ScriptRoot/mkdocs.yml"
+fi
